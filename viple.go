9f84d190bcc66a4d7e9a7b6194fb71e961afd2d9
wearsunscreen <john.crane@gmail.com>
2024-03-27 16:29:06 -0700

    adding animation

--- viple.go
+++ viple.go
@@ -4,8 +4,6 @@ import (
 	"image/color"
 	"log"
 	"math/rand"
-	"strings"
-	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -19,10 +17,11 @@ const (
 	cellSize      = 30
 	numRows       = 11
 	numColumns    = 5
-	blinkInverval = 60 * 2 / 3
+	blinkInverval = 60 / 2
 )
 
 var (
+	brightRed        = color.RGBA{0xfc, 0x10, 0x10, 0xff}
 	lightButter      = color.RGBA{0xfc, 0xe9, 0x4f, 0xff}
 	lightOrange      = color.RGBA{0xfc, 0xaf, 0x3e, 0xff}
 	lightChocolate   = color.RGBA{0xe9, 0xb9, 0x6e, 0xff}
@@ -55,23 +54,36 @@ var (
 	rng        *rand.Rand
 )
 
+type Game struct {
+	cursorSquare Point
+	swapSquare   Point
+	frameCount   int
+	grid         [][]Square
+	keys         []ebiten.Key
+	maxColors    int
+	triplesMask  [][]bool
+}
+
+type Modifier struct {
+	startFrame  int
+	endFrame    int
+	startOffset [2]int
+	endOffset   [2]int
+}
+
+type Square struct {
+	color     int
+	modifiers []Modifier
+	point     Point
+}
+
 type Point struct {
 	x, y int
 }
 
-type Game struct {
-	cursorPoint Point
-	swapPoint   Point
-	frameCount  int
-	grid        [][]int
-	keys        []ebiten.Key
-	maxColors   int
-	triplesMask [][]bool
-}
-
 /* todo
-- detect triples
-- create randdom number generator with seed so I can replay specific games
+- quit with ":q", ":x", ":exit"
+- animation
 */
 
 func main() {
@@ -83,25 +95,16 @@ func main() {
 }
 
 func init() {
-	//source := rand.NewSource(2) // seeding the random number generator can be useful in debugging
-	source := rand.NewSource(time.Now().UnixNano())
+	source := rand.NewSource(3) // seeding the random number generator can be useful in debugging
+	//source := rand.NewSource(time.Now().UnixNano())
 	rng = rand.New(source)
-}
-
-func any(bools []bool) bool {
-	for _, value := range bools {
-		if value {
-			return true
-		}
-	}
-	return false
 }
 
 func detectTriples(g *Game) {
 	// find all horizontal triples
 	for y, row := range g.grid[:len(g.grid)] {
 		for x := range g.grid[:len(row)-2] {
-			if g.grid[y][x] == g.grid[y][x+1] && g.grid[y][x] == g.grid[y][x+2] {
+			if g.grid[y][x].color == g.grid[y][x+1].color && g.grid[y][x].color == g.grid[y][x+2].color {
 				g.triplesMask[y][x], g.triplesMask[y][x+1], g.triplesMask[y][x+2] = true, true, true
 			}
 		}
@@ -110,7 +113,7 @@ func detectTriples(g *Game) {
 	// find all vertical triples
 	for y, row := range g.grid[:len(g.grid)-2] {
 		for x := range g.grid[:len(row)] {
-			if g.grid[y][x] == g.grid[y+1][x] && g.grid[y][x] == g.grid[y+2][x] {
+			if g.grid[y][x].color == g.grid[y+1][x].color && g.grid[y][x].color == g.grid[y+2][x].color {
 				g.triplesMask[y][x], g.triplesMask[y+1][x], g.triplesMask[y+2][x] = true, true, true
 			}
 		}
@@ -127,8 +130,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	// draw cells
 	for y, row := range g.grid {
-		for x, col := range row {
-			vector.DrawFilledRect(screen, float32(cellSize*x+margin+2), float32(cellSize*y+margin+2), cellSize-4, cellSize-4, gameColors[col], false)
+		for x, _ := range row {
+			DrawSquare(screen, &g.grid[y][x], g.frameCount)
 		}
 	}
 
@@ -142,29 +145,43 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 
 	// draw cursor
-	cursorColor := color.White
-	if g.frameCount/blinkInverval%2 == 1 {
-		cursorColor = color.Black
+	cursorColors := [2]color.Color{color.White, color.Black}
+	blink := g.frameCount / blinkInverval % 2
+	var cursorWidth float32 = 4
+	if g.swapSquare.x != -1 {
+		// we are in swap mode, faster blink, brighter colors
+		blink = g.frameCount / (blinkInverval / 2) % 2
+		cursorColors = [2]color.Color{brightRed, lightButter}
+		cursorWidth = 6
 	}
-	vector.StrokeRect(screen, float32(cellSize*g.cursorPoint.x+margin), float32(cellSize*g.cursorPoint.y+margin),
-		cellSize, cellSize, 4, cursorColor, false)
-
-	// draw pressed keys
-	var keyStrs []string
-	var keyNames []string
-	for _, k := range g.keys {
-		keyStrs = append(keyStrs, k.String())
-		if name := ebiten.KeyName(k); name != "" {
-			keyNames = append(keyNames, name)
-		}
-	}
-	ebitenutil.DebugPrint(screen, strings.Join(keyStrs, ", ")+"\n"+strings.Join(keyNames, ", "))
+	vector.StrokeRect(screen, float32(cellSize*g.cursorSquare.x+margin), float32(cellSize*g.cursorSquare.y+margin),
+		cellSize, cellSize, cursorWidth, cursorColors[blink], false)
 }
 
-func fillRandom(arr [][]int, upTo int) {
-	for i := range arr {
-		for j := range arr[i] {
-			arr[i][j] = rng.Intn(upTo) // Generate random number between 0 (inclusive) and upTo (exclusive)
+func applyModifier(m *Modifier, op *ebiten.DrawImageOptions, frameCount int) {
+	var completionRatio float64
+	completionRatio = float64(m.endFrame-frameCount) / float64(m.endFrame-m.startFrame)
+	op.GeoM.Translate(
+		float64(m.startOffset[0])+(completionRatio*float64(m.endOffset[0]-m.startOffset[0])),
+		float64(m.startOffset[1])+(completionRatio*float64(m.endOffset[0]-m.startOffset[1])))
+}
+
+func DrawSquare(screen *ebiten.Image, square *Square, frameCount int) {
+	//vector.DrawFilledRect(screen, float32(cellSize*x+margin+2), float32(cellSize*y+margin+2), cellSize-4, cellSize-4, gameColors[color], false)
+	rect := ebiten.NewImage(cellSize-4, cellSize-4)
+	rect.Fill(gameColors[square.color])
+	op := &ebiten.DrawImageOptions{}
+	for _, m := range square.modifiers {
+		applyModifier(&m, op, frameCount)
+	}
+	op.GeoM.Translate(cellSize*float64(square.point.x)+margin+2, cellSize*float64(square.point.y)+margin+2)
+	screen.DrawImage(rect, op)
+}
+
+func fillRandom(g *Game, upTo int) {
+	for y, row := range g.grid {
+		for x := range row {
+			g.grid[y][x].color = rng.Intn(upTo)
 		}
 	}
 }
@@ -180,14 +197,20 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 
 func newGame() *Game {
 	g := Game{
-		maxColors:   5,
-		cursorPoint: Point{numColumns / 2, numRows / 2},
-		swapPoint:   Point{-1, -1},
+		maxColors:    5,
+		cursorSquare: Point{numColumns / 2, numRows / 2},
+		swapSquare:   Point{-1, -1},
 	}
 
-	g.grid = make([][]int, numRows)
-	for i := range g.grid {
-		g.grid[i] = make([]int, numColumns)
+	g.grid = make([][]Square, numRows)
+	for y := range g.grid {
+		g.grid[y] = make([]Square, numColumns)
+	}
+
+	for y, row := range g.grid {
+		for x, _ := range row {
+			g.grid[y][x].point = Point{x, y}
+		}
 	}
 
 	g.triplesMask = make([][]bool, numRows)
@@ -195,53 +218,82 @@ func newGame() *Game {
 		g.triplesMask[i] = make([]bool, numColumns)
 	}
 
-	fillRandom(g.grid, 6)
+	fillRandom(&g, 6)
 	return &g
 }
 
-// Exchange positions of two neighboring points, return false if unable to exchange.
+// Exchange positions of two neighboring squares, return false if unable to exchange.
 // The exchange fails if swap point and cursor point are the same (this can happen when
 // player attempts to move off the grid). The exchange fails if both points have the
 // same value.
-func SwapPoints(g *Game) bool {
-	if g.swapPoint == g.cursorPoint {
-		g.swapPoint = Point{-1, -1} // indicates we are no longer attempting to swap
+func SwapSquares(g *Game) bool {
+	if g.swapSquare == g.cursorSquare {
+		g.swapSquare = Point{-1, -1} // indicates we are no longer attempting to swap
 		return false
 	}
-	if g.grid[g.swapPoint.y][g.swapPoint.x] == g.grid[g.cursorPoint.y][g.cursorPoint.x] {
-		g.swapPoint = Point{-1, -1} // indicates we are no longer attempting to swap
+	if g.grid[g.swapSquare.y][g.swapSquare.x].point == g.grid[g.cursorSquare.y][g.cursorSquare.x].point {
+		g.swapSquare = Point{-1, -1} // indicates we are no longer attempting to swap
 		return false
 	}
 
-	temp := g.grid[g.swapPoint.y][g.swapPoint.x]
-	g.grid[g.swapPoint.y][g.swapPoint.x] = g.grid[g.cursorPoint.y][g.cursorPoint.x]
-	g.grid[g.cursorPoint.y][g.cursorPoint.x] = temp
-	g.swapPoint = Point{-1, -1} // indicates we are no longer attempting to swap
+	temp := g.grid[g.swapSquare.y][g.swapSquare.x].color
+	g.grid[g.swapSquare.y][g.swapSquare.x].color = g.grid[g.cursorSquare.y][g.cursorSquare.x].color
+	g.grid[g.cursorSquare.y][g.cursorSquare.x].color = temp
+
+	// Bugbug: this will delete all existing modifiers
+	// add animation
+	mover := Modifier{
+		startFrame:  g.frameCount,
+		endFrame:    g.frameCount + 120,
+		startOffset: [2]int{20, 20},
+		endOffset:   [2]int{40, 40}}
+
+	newModifiers := []Modifier{mover}
+
+	g.grid[g.swapSquare.y][g.swapSquare.x].modifiers = newModifiers
+	g.grid[g.swapSquare.y][g.swapSquare.x].modifiers = newModifiers
+
+	g.swapSquare = Point{-1, -1} // indicates we are no longer attempting to swap
 	return true
 }
 
 func (g *Game) Update() error {
 	g.frameCount++
+	// clear all expired Modifiers
+	for y, row := range g.grid {
+		for x, _ := range row {
+			if len(g.grid[y][x].modifiers) > 0 {
+				newModifiers := []Modifier{}
+				for _, m := range g.grid[y][x].modifiers {
+					if g.frameCount < m.endFrame {
+						newModifiers = append(newModifiers, m)
+					}
+				}
+				g.grid[y][x].modifiers = newModifiers
+			}
+		}
+	}
+
 	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
 	for _, k := range g.keys {
 		if inpututil.IsKeyJustPressed(k) {
 			switch k {
 			case ebiten.KeyJ:
-				g.cursorPoint.x = max(g.cursorPoint.x-1, 0)
+				g.cursorSquare.x = max(g.cursorSquare.x-1, 0)
 			case ebiten.KeyL:
-				g.cursorPoint.x = min(g.cursorPoint.x+1, numColumns-1)
+				g.cursorSquare.x = min(g.cursorSquare.x+1, numColumns-1)
 			case ebiten.KeyI:
-				g.cursorPoint.y = max(g.cursorPoint.y-1, 0)
+				g.cursorSquare.y = max(g.cursorSquare.y-1, 0)
 			case ebiten.KeyK:
-				g.cursorPoint.y = min(g.cursorPoint.y+1, numRows-1)
-			case ebiten.KeySpace:
-				if g.swapPoint.x == -1 {
+				g.cursorSquare.y = min(g.cursorSquare.y+1, numRows-1)
+			case ebiten.KeyV:
+				if g.swapSquare.x == -1 {
 					// initiating a swap
-					g.swapPoint = g.cursorPoint
+					g.swapSquare = g.cursorSquare
 				}
 			}
-			if g.swapPoint.x != -1 && g.swapPoint != g.cursorPoint {
-				if result := SwapPoints(g); !result {
+			if g.swapSquare.x != -1 && g.swapSquare != g.cursorSquare {
+				if result := SwapSquares(g); !result {
 					// TODO: play a buzzer noise to indicate failure
 					log.Println("BUZZZ")
 				}
