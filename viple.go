c58aaa39c2cc78b1a6aca0daeac521cfadea61b4
wearsunscreen <john.crane@gmail.com>
2024-04-04 19:29:21 -0700

    moved files into resources folder

--- viple.go
+++ viple.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"image/color"
 	_ "image/png"
 	"log"
 	"math/rand"
@@ -28,37 +27,7 @@ const (
 	version       = "Viple 0.1"
 )
 
-// colors from the Tango Desktop Project
 var (
-	brightRed        = color.RGBA{0xfc, 0x10, 0x10, 0xff}
-	lightButter      = color.RGBA{0xfc, 0xe9, 0x4f, 0xff}
-	lightOrange      = color.RGBA{0xfc, 0xaf, 0x3e, 0xff}
-	lightChocolate   = color.RGBA{0xe9, 0xb9, 0x6e, 0xff}
-	lightGreen       = color.RGBA{0x8a, 0xe2, 0x34, 0xff}
-	lightSkyBlue     = color.RGBA{0x72, 0x9f, 0xcf, 0xff}
-	lightPlum        = color.RGBA{0xad, 0x7f, 0xa8, 0xff}
-	lightScarletRed  = color.RGBA{0xef, 0x29, 0x29, 0xff}
-	lightAluminium   = color.RGBA{0xee, 0xee, 0xec, 0xff}
-	lightCoal        = color.RGBA{0x88, 0x8a, 0x85, 0xff}
-	mediumButter     = color.RGBA{0xed, 0xd4, 0x00, 0xff}
-	mediumOrange     = color.RGBA{0xf5, 0x79, 0x00, 0xff}
-	mediumChocolate  = color.RGBA{0xc1, 0x7d, 0x11, 0xff}
-	mediumGreen      = color.RGBA{0x73, 0xd2, 0x16, 0xff}
-	mediumSkyBlue    = color.RGBA{0x34, 0x65, 0xa4, 0xff}
-	mediumPlum       = color.RGBA{0x75, 0x50, 0x7b, 0xff}
-	mediumScarletRed = color.RGBA{0xcc, 0x00, 0x00, 0xff}
-	mediumAluminium  = color.RGBA{0xd3, 0xd7, 0xcf, 0xff}
-	mediumCoal       = color.RGBA{0x55, 0x57, 0x53, 0xff}
-	darkButter       = color.RGBA{0xc4, 0xa0, 0x00, 0xff}
-	darkOrange       = color.RGBA{0xce, 0x5c, 0x00, 0xff}
-	darkChocolate    = color.RGBA{0x8f, 0x59, 0x02, 0xff}
-	darkGreen        = color.RGBA{0x4e, 0x9a, 0x06, 0xff}
-	darkSkyBlue      = color.RGBA{0x20, 0x4a, 0x87, 0xff}
-	darkPlum         = color.RGBA{0x5c, 0x35, 0x66, 0xff}
-	darkScarletRed   = color.RGBA{0xa4, 0x00, 0x00, 0xff}
-	darkAluminium    = color.RGBA{0xba, 0xbd, 0xb6, 0xff}
-	darkCoal         = color.RGBA{0x2e, 0x34, 0x36, 0xff}
-
 	rng *rand.Rand
 )
 
@@ -93,31 +62,8 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	// draw background
 	screen.Fill(mediumCoal)
 
-	// draw background of triples
-	for y, row := range g.grid {
-		for x := range row {
-			if g.triplesMask[y][x] {
-				g.grid[y][x].DrawBackground(screen, darkButter)
-			}
-		}
-	}
+	DrawGrid(screen, g)
 
-	// draw cursor
-	cursorColors := [2]color.Color{color.White, color.Black}
-	blink := g.frameCount / blinkInverval % 2
-	if g.swapSquare.x != -1 {
-		// we are in swap mode, faster blink, brighter colors
-		blink = g.frameCount / (blinkInverval / 2) % 2
-		cursorColors = [2]color.Color{brightRed, lightButter}
-	}
-	g.grid[g.cursorSquare.y][g.cursorSquare.x].DrawBackground(screen, cursorColors[blink])
-
-	// draw gems
-	for y, row := range g.grid {
-		for x, _ := range row {
-			g.grid[y][x].DrawGem(screen, g.gemImages[g.grid[y][x].color], g.frameCount)
-		}
-	}
 }
 
 func gameDimensions() (width int, height int) {
@@ -140,7 +86,7 @@ func loadImage(path string) *ebiten.Image {
 func loadImages(g *Game) {
 	g.gemImages = make([]*ebiten.Image, g.numColors)
 	for i := range g.numColors {
-		image := loadImage("Gem " + strconv.Itoa(i+1) + ".png")
+		image := loadImage("resources/Gem " + strconv.Itoa(i+1) + ".png")
 		g.gemImages[i] = image
 	}
 }
@@ -249,13 +195,13 @@ func (g *Game) Update() error {
 	for _, k := range g.keys {
 		if inpututil.IsKeyJustPressed(k) {
 			switch k {
-			case ebiten.KeyJ:
+			case ebiten.KeyH:
 				g.cursorSquare.x = max(g.cursorSquare.x-1, 0)
 			case ebiten.KeyL:
 				g.cursorSquare.x = min(g.cursorSquare.x+1, numColumns-1)
-			case ebiten.KeyI:
-				g.cursorSquare.y = max(g.cursorSquare.y-1, 0)
 			case ebiten.KeyK:
+				g.cursorSquare.y = max(g.cursorSquare.y-1, 0)
+			case ebiten.KeyJ:
 				g.cursorSquare.y = min(g.cursorSquare.y+1, numRows-1)
 			case ebiten.KeyV:
 				if g.swapSquare.x == -1 {
