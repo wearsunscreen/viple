0b97de4da6ca43692684e3832f03fa130ee89323
John Crane <john.crane@gmail.com>
2024-05-23 16:28:01 -0700

    adding wasm and htmtl files to run as wasm on a web page

---
+++ main_fs.go
@@ -0,0 +1,26 @@
+// Description: This is the main file for the file server. It creates a file server
+// that serves the contents of the specified directory and starts the server on port 8080.
+//
+// To use,
+// - put in a separate directory,
+// - rename main_fs.go to main.go,
+// - rename main_fs function to main,
+// - rename the package to main
+// - set the directory to serve in the http.Dir() function.
+package main_fs
+
+import (
+	"log"
+	"net/http"
+)
+
+func main_fs() {
+	// Create a file server that serves the contents of the specified directory.
+	fileServer := http.FileServer(http.Dir("../viple"))
+
+	// Register the file server as the handler for all requests.
+	http.Handle("/", fileServer)
+
+	// Start the server on port 8080.
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
