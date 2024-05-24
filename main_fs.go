1bee86b9c8dfc1e804a616be35ebf605faabc9f2
John Crane <john.crane@gmail.com>
2024-05-23 17:48:27 -0700

    changes to make main_fs.go more readable

--- main_fs.go
+++ main_fs.go
@@ -1,30 +1,28 @@
-package main
-
-// Description: This is the main file for the file server. It creates a file server
-// that serves the contents of the specified directory and starts the server on port 8080.
-//
+// Description: This is a simple file server which is needed to run the page with
+// wasm locally, the browser will not load wasm files.
 // To use,
 // - put in a separate directory,
 // - uncomment code below
 // - rename main_fs.go to main.go,
 // - rename main_fs function to main,
-// - rename the package to main
 // - set the directory to serve in the http.Dir() function.
+// - run go mod init <any_module_name>
+// - run go mod tidy
+// - run the program.
+package main
 
-// package main_fs
+import (
+	"log"
+	"net/http"
+)
 
-// import (
-// 	"log"
-// 	"net/http"
-// )
+func main_fs() {
+	// Create a file server that serves the contents of the specified directory.
+	fileServer := http.FileServer(http.Dir("../viple"))
 
-// func main_fs() {
-// 	// Create a file server that serves the contents of the specified directory.
-// 	fileServer := http.FileServer(http.Dir("../viple"))
+	// Register the file server as the handler for all requests.
+	http.Handle("/", fileServer)
 
-// 	// Register the file server as the handler for all requests.
-// 	http.Handle("/", fileServer)
-
-// 	// Start the server on port 8080.
-// 	log.Fatal(http.ListenAndServe(":8080", nil))
-// }
+	// Start the server on port 8080.
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
